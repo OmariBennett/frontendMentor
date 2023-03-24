283fab9e0437c9e44b0bb2110cb47fa1ea945c44
OmariBennett <omari.bennett@gmail.com>
2023-03-24 11:56:18 -0400

    putting the progect together

--- src/project/resultsSummary/index.svelte
+++ src/project/resultsSummary/index.svelte
@@ -1,8 +1,5 @@
 <svelte:head>
-	<link
-		href="https://fonts.googleapis.com/css2?family=Caveat&family=Hanken+Grotesk:wght@500;700;800&family=Open+Sans:wght@300&display=swap"
-		rel="stylesheet"
-	/>
+	<link rel="stylesheet" />
 	<title>Frontend Mentor | Results summary component</title>
 </svelte:head>
 
@@ -34,57 +31,45 @@
 </div>
 
 <style>
-	/* 
-# Front-end Style Guide
+	/* The designs were created to the following widths: */
+	/* - Mobile: 375px */
+	/* - Desktop: 1440px */
+	main {
+		/* ## Colors */
+		/* ### Primary */
+		--accent-color-1: hsl(0, 100%, 67%); /* Light red */
+		--accent-color-2: hsl(39, 100%, 56%); /* Orangey yellow */
+		--accent-color-3: hsl(166, 100%, 37%); /* Green teal */
+		--accent-color-4: hsl(234, 85%, 45%); /* Cobalt blue */
 
-## Layout
+		/* ## Gradients */
+		/* Light slate blue (background) to Light royal blue (background) */
+		--gradient-1: hsl(252, 100%, 67%), hsl(252, 100%, 67%);
+		/* Violet blue (circle) to Persian blue (circle) */
+		--gradient-2: hsla(256, 72%, 46%, 1), hsla(241, 72%, 46%, 0);
 
-The designs were created to the following widths:
+		/* ### Neutral */
+		--neutral-hsl-color-100: hsl(0, 0%, 100%); /* White */
+		--neutral-hsl-color-200: hsl(221, 100%, 96%); /* Pale blue */
+		--neutral-hsl-color-400: hsl(241, 100%, 89%); /* Light lavender */
+		--neutral-hsl-color-900: hsl(224, 30%, 27%); /* Dark gray blue */
 
-- Mobile: 375px
-- Desktop: 1440px
-
-## Colors
-
-### Primary
-
-- Light red: hsl(0, 100%, 67%)
-- Orangey yellow: hsl(39, 100%, 56%)
-- Green teal: hsl(166, 100%, 37%)
-- Cobalt blue: hsl(234, 85%, 45%)
-
-## Gradients
-
-- Light slate blue (background): hsl(252, 100%, 67%)
-- Light royal blue (background): hsl(241, 81%, 54%)
-
-- Violet blue (circle): hsla(256, 72%, 46%, 1)
-- Persian blue (circle): hsla(241, 72%, 46%, 0)
-
-
-
-### Neutral
-
-- White: hsl(0, 0%, 100%)
-- Pale blue: hsl(221, 100%, 96%)
-- Light lavender: hsl(241, 100%, 89%)
-- Dark gray blue: hsl(224, 30%, 27%)
-
-### Notes
-
-Use transparency to get the colour variations
-necessary to match the design. Hint: look into
-using `hsla()`.
-
-## Typography
-
-### Body Copy
-
-- Font size (paragraphs): 18px
-
-### Font
-
-- Family: [Hanken Grotesk](https://fonts.google.com/specimen/Hanken+Grotesk)
-- Weights: 500, 700, 800
-   */
+		/* ## Typography */
+		/* - Family: [Hanken Grotesk](https://fonts.google.com/specimen/Hanken+Grotesk) */
+		/* - Weights: 500, 700, 800 */
+		@font-face {
+			font-family: 'Hanken Grotesk';
+			font-display: swap;
+			font-weight: 100 900;
+			src: local('Hanken Grotesk'),
+				url('../../../projects/results-summary-component-main/assets/fonts/HankenGrotesk-VariableFont_wght.ttf')
+					format('TrueType'),
+				url('https://fonts.googleapis.com/css2?family=Caveat&family=Hanken+Grotesk:wght@500;700;800&family=Open+Sans:wght@300&display=swap')
+					format('TrueType');
+		}
+	}
+	:global(body) {
+		font-size: 1.125rem; /* - Font size (paragraphs): 18px */
+		font-family: 'Hanken Grotesk';
+	}
 </style>
